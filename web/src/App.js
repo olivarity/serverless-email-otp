2b13cc5759a3dfd6fdfb5e9c1b5163a767d48a8f
Olivia Isaacs <olivia@scaasic.com>
2022-10-26 16:24:11 -0500

    Basic happy path UI

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TextInput from './components/TextInput';
 
-function App() {
+export default function App() {
+  const [ email, updateEmail ] = useState();
+  const [ auth, updateAuth ] = useState();
+
+  async function requestEmail(emailValue) {
+    const res = await fetch('https://om6evmhe19.execute-api.us-east-1.amazonaws.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: emailValue })
+    });
+    updateEmail(emailValue);
+  }
+
+  async function attemptChallenge(codeValue) {
+    const res = await fetch('https://om6evmhe19.execute-api.us-east-1.amazonaws.com/auth/login/challenge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, code: codeValue })
+    });
+    const payload = await res.json();
+    console.log(payload);
+
+    updateAuth(payload.token);
+  }
+
+  function buildPage() {
+    if(auth) {
+      return <p>Super Secret Page</p>
+    } 
+    else if(email) {
+      return (
+        <>
+          <p>Logging in as {email}. Please enter code from email.</p>
+          <TextInput 
+            label="OTP"
+            onSubmit={attemptChallenge}
+            type="tel" 
+            pattern="\d{6}"
+            placeholder="######" 
+            size="6"
+          />
+        </>
+      );
+    }
+    else {
+      return (
+        <TextInput 
+          label="Email" 
+          type="email" 
+          onSubmit={requestEmail} 
+        />
+      );
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { buildPage() }
     </div>
   );
 }
-
-export default App;
---
+++ web/src/components/TextInput.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function TextInput({ label, onSubmit, ...props }) {
+    const [value, setValue] = useState('');
+
+    function handleInputChange(e) {
+        setValue(e.target.value);
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        onSubmit(value);
+    }
+
+    return(
+        <form onSubmit={handleFormSubmit}>
+            <label>{label}</label>
+            <input 
+                value={value} 
+                onChange={handleInputChange}
+                {...props}
+            />
+            <input type="submit" />
+        </form>
+    );
+}
