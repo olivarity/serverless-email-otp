ae1c2616fd7e64882d1dc7d52d3582c6299d00f1
Olivia Isaacs <olivia@scaasic.com>
2022-10-25 21:42:47 -0500

    Issue and validate JWT

--- api/functions/auth.js
+++ api/functions/auth.js
@@ -1,17 +1,18 @@
 'use strict';
 const AWS = require('aws-sdk');
 const { randomInt } = require('node:crypto');
+const jwt = require('jsonwebtoken');
+
+const OTP_TTL = 15; // in minutes
 
 const db = new AWS.DynamoDB.DocumentClient();
 const ses = new AWS.SES();
 
 module.exports.sendEmail = async (event) => {
-    const otpTtl = 15; // in minutes
-
     const body = JSON.parse(event.body);
 
     const otpCode = randomInt(999999).toString().padStart(6, '0');
-    const expTime = new Date(Date.now() + otpTtl * 60000).toISOString();
+    const expTime = new Date(Date.now() + OTP_TTL * 60000).toISOString();
 
     const dbParams = {
         TableName: process.env.DYNAMODB_TABLE,
@@ -36,7 +37,7 @@ module.exports.sendEmail = async (event) => {
         Source: process.env.SENDER_ADDRESS
     };
 
-    await ses.sendEmail(sesParams).promise();
+    // await ses.sendEmail(sesParams).promise();
 
     return { statusCode: 200, body: `Email sent. Code expires at ${expTime}` }
 }
@@ -57,7 +58,11 @@ module.exports.validateCode = async (event) => {
     if(record.codeExp && new Date(record.codeExp) > new Date()) {
         if(record.code == body.code) {
             await clearCode(body.email);
-            return { statusCode: 200, body: "success, issue auth token" }
+            const token = jwt.sign(
+                { user: body.email }, 
+                process.env.JWT_SECRET, 
+                { expiresIn: 3600 });
+            return { statusCode: 200, body: JSON.stringify({ token })}
         }
 
         return { statusCode: 400, body: "failure, increment retries"}
@@ -75,4 +80,32 @@ const clearCode = async (email) => {
     }
 
     return await db.update(dbParams).promise();
+}
+
+module.exports.validateToken = (event, context, callback) => {
+    console.log(JSON.stringify(event));
+    const token = event.authorizationToken;
+    try {
+        const decodedToken = jwt.verify(event.headers.authorization, process.env.JWT_SECRET);
+        const policyDocument = {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: 'Allow',
+                    Resource: event.routeArn
+                }
+            ]
+        }
+        const response = {
+            principalId: 'user',
+            policyDocument,
+            context: decodedToken
+        }
+        console.log(JSON.stringify(response));
+        callback(null, response);
+    } catch {
+        callback("Unauthorized");
+    }
+    
 }
